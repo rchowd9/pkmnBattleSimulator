88be65b725299c589a5a4cd0f6783c15fdfcc637
rchowd9 <135185247+rchowd9@users.noreply.github.com>
2025-07-09 17:31:30 -0400

    Update battle.tsx

--- PokemonBattleSimulator/PokemonBattleSimulator/client/src/pages/battle.tsx
+++ PokemonBattleSimulator/PokemonBattleSimulator/client/src/pages/battle.tsx
@@ -50,12 +50,12 @@ export default function Battle() {
     'Charizard': { 
       type: 'Fire', 
       moves: ['Flamethrower', 'Air Slash', 'Dragon Claw', 'Earthquake'],
-      megaMoves: ['Mega Flamethrower', 'Mega Air Slash', 'Dragon Claw', 'Earthquake']
+      megaMoves: ['Mega Flamethrower', 'Air Slash', 'Dragon Claw', 'Earthquake']
     },
     'Blastoise': { 
       type: 'Water', 
       moves: ['Hydro Pump', 'Ice Beam', 'Skull Bash', 'Flash Cannon'],
-      megaMoves: ['Mega Hydro Pump', 'Mega Ice Beam', 'Skull Bash', 'Flash Cannon']
+      megaMoves: ['Mega Hydro Pump', 'Ice Beam', 'Skull Bash', 'Flash Cannon']
     },
     'Venusaur': { 
       type: 'Grass', 
@@ -69,58 +69,61 @@ export default function Battle() {
     }
   };
 
-  // Type effectiveness chart - expanded with more realistic matchups
+  // Type effectiveness chart - based on official Bulbapedia type chart
   const typeEffectiveness: { [key: string]: { [key: string]: number } } = {
+    'Normal': { 
+      Rock: 0.5, Ghost: 0.0, Steel: 0.5 
+    },
     'Fire': { 
-      Grass: 2.0, Water: 0.5, Fire: 0.5, Ice: 2.0, Steel: 2.0, Bug: 2.0 
+      Fire: 0.5, Water: 0.5, Grass: 2.0, Ice: 2.0, Bug: 2.0, Rock: 0.5, Dragon: 0.5, Steel: 2.0 
     },
     'Water': { 
-      Fire: 2.0, Grass: 0.5, Water: 0.5, Ground: 2.0, Rock: 2.0, Dragon: 0.5 
-    },
-    'Grass': { 
-      Water: 2.0, Fire: 0.5, Grass: 0.5, Ground: 2.0, Rock: 2.0, Dragon: 0.5, Flying: 0.5, Poison: 0.5, Bug: 0.5, Steel: 0.5 
+      Fire: 2.0, Water: 0.5, Grass: 0.5, Ground: 2.0, Rock: 2.0, Dragon: 0.5 
     },
     'Electric': { 
-      Water: 2.0, Grass: 0.5, Electric: 0.5, Ground: 0.0, Flying: 2.0, Dragon: 0.5 
+      Water: 2.0, Electric: 0.5, Grass: 0.5, Ground: 0.0, Flying: 2.0, Dragon: 0.5 
+    },
+    'Grass': { 
+      Fire: 0.5, Water: 2.0, Grass: 0.5, Poison: 0.5, Ground: 2.0, Flying: 0.5, Bug: 0.5, Rock: 2.0, Dragon: 0.5, Steel: 0.5 
     },
     'Ice': { 
-      Grass: 2.0, Fire: 0.5, Water: 0.5, Ground: 2.0, Flying: 2.0, Dragon: 2.0, Steel: 0.5 
+      Fire: 0.5, Water: 0.5, Grass: 2.0, Ice: 0.5, Ground: 2.0, Flying: 2.0, Dragon: 2.0, Steel: 0.5 
     },
     'Fighting': { 
-      Normal: 2.0, Ice: 2.0, Rock: 2.0, Steel: 2.0, Dark: 2.0, Flying: 0.5, Poison: 0.5, Psychic: 0.5, Bug: 0.5, Ghost: 0.0, Fairy: 0.5 
+      Normal: 2.0, Ice: 2.0, Poison: 0.5, Flying: 0.5, Psychic: 0.5, Bug: 0.5, Rock: 2.0, Ghost: 0.0, Steel: 2.0, Fairy: 0.5 
     },
     'Poison': { 
-      Grass: 2.0, Fairy: 2.0, Poison: 0.5, Ground: 0.5, Rock: 0.5, Ghost: 0.5, Steel: 0.0 
+      Grass: 2.0, Poison: 0.5, Ground: 0.5, Rock: 0.5, Ghost: 0.5, Steel: 0.0, Fairy: 2.0 
     },
     'Ground': { 
-      Fire: 2.0, Electric: 2.0, Grass: 0.5, Poison: 2.0, Rock: 2.0, Steel: 2.0, Bug: 0.5, Flying: 0.0 
+      Fire: 2.0, Electric: 2.0, Grass: 0.5, Poison: 2.0, Flying: 0.0, Bug: 0.5, Rock: 2.0, Steel: 2.0 
     },
     'Flying': { 
-      Grass: 2.0, Fighting: 2.0, Bug: 2.0, Electric: 0.5, Rock: 0.5, Steel: 0.5 
+      Electric: 0.5, Grass: 2.0, Fighting: 2.0, Bug: 2.0, Rock: 0.5, Steel: 0.5 
     },
     'Psychic': { 
       Fighting: 2.0, Poison: 2.0, Psychic: 0.5, Dark: 0.0, Steel: 0.5 
     },
     'Bug': { 
-      Grass: 2.0, Psychic: 2.0, Dark: 2.0, Fire: 0.5, Fighting: 0.5, Poison: 0.5, Flying: 0.5, Ghost: 0.5, Steel: 0.5, Fairy: 0.5 
+      Fire: 0.5, Grass: 2.0, Fighting: 0.5, Poison: 0.5, Flying: 0.5, Psychic: 2.0, Ghost: 0.5, Steel: 0.5, Dark: 2.0, Fairy: 0.5 
     },
     'Rock': { 
-      Fire: 2.0, Ice: 2.0, Flying: 2.0, Bug: 2.0, Fighting: 0.5, Ground: 0.5, Steel: 0.5 
+      Fire: 2.0, Ice: 2.0, Fighting: 0.5, Ground: 0.5, Flying: 2.0, Bug: 2.0, Steel: 0.5 
     },
     'Ghost': { 
-      Psychic: 2.0, Ghost: 2.0, Dark: 0.5, Normal: 0.0 
+      Normal: 0.0, Psychic: 2.0, Ghost: 2.0, Dark: 0.5 
     },
     'Dragon': { 
       Dragon: 2.0, Steel: 0.5, Fairy: 0.0 
     },
     'Dark': { 
-      Psychic: 2.0, Ghost: 2.0, Fighting: 0.5, Dark: 0.5, Fairy: 0.5 
+      Fighting: 0.5, Psychic: 2.0, Ghost: 2.0, Dark: 0.5, Fairy: 0.5 
     },
     'Steel': { 
-      Ice: 2.0, Rock: 2.0, Fairy: 2.0, Fire: 0.5, Water: 0.5, Electric: 0.5, Steel: 0.5 
+      Fire: 0.5, Water: 0.5, Electric: 0.5, Ice: 2.0, Rock: 2.0, Steel: 0.5, Fairy: 2.0 
     },
     'Fairy': { 
-      Fighting: 2.0, Dragon: 2.0, Dark: 2.0, Fire: 0.5, Poison: 0.5, Steel: 0.5 
+      Fighting: 2.0, Poison: 0.5, Dragon: 2.0, Dark: 2.0, Steel: 0.5 
     }
   };
 
@@ -470,4 +473,4 @@ export default function Battle() {
       </div>
     </div>
   );
-} 
+}  
